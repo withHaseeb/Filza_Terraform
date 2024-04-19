6b902c11ccb4d40e68b0f8a7b1c90cedc154059c
filzaromaan12 <117765972+filzaromaan12@users.noreply.github.com>
2024-04-19 00:01:51 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "filza-infra-tf1-state" 
+    bucket         = "filza-infra-tf10-state" 
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"
